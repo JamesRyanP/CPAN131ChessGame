c8dbc8ec8625fca8278bef9761d2415995b985a9
JamesRyanP <101903402+JamesRyanP@users.noreply.github.com>
2022-07-26 17:53:52 -0400

    Add files via upload

--- CPAN131ChessGame/src/cpan131chessgame/Bishop.java
+++ CPAN131ChessGame/src/cpan131chessgame/Bishop.java
@@ -5,10 +5,34 @@ public class Bishop extends Tile{
     
     // pieceColor is enum for either "BLACK" or "WHITE"
     Player pieceColor;
+    protected int currentX;
+    protected int currentY;
     
-    public Bishop(String value, Player pieceColor){
+    public Bishop(String value, Player pieceColor, int X, int Y){
         super(value);
         this.pieceColor = pieceColor;
+        currentX = X;
+        currentY = Y;
+    }
+
+    public Player getPieceColor() {
+        return pieceColor;
+    }
+
+    public int getCurrentX() {
+        return currentX;
+    }
+
+    public int getCurrentY() {
+        return currentY;
+    }
+
+    public void setCurrentX(int currentX) {
+        this.currentX = currentX;
+    }
+
+    public void setCurrentY(int currentY) {
+        this.currentY = currentY;
     }
     
     
